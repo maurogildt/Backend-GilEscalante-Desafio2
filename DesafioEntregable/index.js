dc69190730209d2c89a8a2c8f8850aef9926a052
gil mauro <gil_mauro@hotmail.com>
2023-04-09 18:52:34 -0300

    desafio 2023-04-10

---
+++ DesafioEntregable/index.js
@@ -0,0 +1,28 @@
+import ProductManager from "./managers/ProductManager.js";
+
+const productManager = new ProductManager()
+
+const context = async () => {
+    console.log(await productManager.getProducts())
+
+    await productManager.addProducts({
+        title: "producto prueba",
+        description: "Este es un producto prueba",
+        price: 200,
+        thumbnail: "Sin imagen",
+        code: "abc123",
+        stock: 25
+    })
+
+    console.log(await productManager.getProducts())
+
+    console.log(await productManager.getProductById(1))
+
+    console.log(await productManager.updateProduct(1,{price: 500, stock: 1}))
+    console.log(await productManager.getProducts())
+
+    console.log(await productManager.deleteProduct(1))
+    console.log(await productManager.getProducts())
+}
+
+context()
